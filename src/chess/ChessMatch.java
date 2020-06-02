690264796f1d39321059a013a5e70ede9dd46375
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2020-06-02 16:21:28 -0300

    Created BoardExeception class plus additioned methods in boardGame class (thereIsAPiece, position exists)

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,5 +27,6 @@ public class ChessMatch {
 	private void initialSetup() {
 		board.PlacePiece(new Rook(board, Color.WITHE), new Position(2, 1));
 		board.PlacePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.PlacePiece(new King(board, Color.BLACK), new Position(7, 4));
 	}
 }
